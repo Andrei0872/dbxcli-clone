f5dd8c448adc404d6fcb8c649d1863b39ea21e36
Andrei Gatej <andreigtj01@gmail.com>
2022-01-10 18:21:59 +0200

    feat: read commands continously until user exists

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "src/execute_command.h"
 
-int main () {
-  printf("hello!! \n");
-  executeCommand();
+int main (int argC, char *args[]) {
+  char *rawCommand = NULL;
+  size_t len = 0;
+  ssize_t read;
+
+  // TODO: ensure `dropbox` created
+
+  while ((read = getline(&rawCommand, &len, stdin)) != -1) {
+    if (read <= 0) {
+      continue;
+    }
+
+    if (strstr(rawCommand, "exit")) {
+      break;
+    }
+
+    executeCommand(rawCommand);
+  }
+
+  free(rawCommand);
+
+  return 0;
 }
