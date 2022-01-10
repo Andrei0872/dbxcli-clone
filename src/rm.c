3eddc260e473d60dfd2367ce6f0f3a07cb4ad92e
Andrei Gatej <andreigtj01@gmail.com>
2022-01-10 22:32:32 +0200

    fix(rm): allow removing non-empty directories

--- src/rm.c
+++ src/rm.c
@@ -2,20 +2,39 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 void rmCommand (char* directoryOrFile) {
-  directoryOrFile[strlen(directoryOrFile) - 1] = '\0';
   char *fullPath = strcat(strcat(realpath("dropbox", NULL), "/"), directoryOrFile);
   printf("Removing: %s\n", fullPath);
 
   int isOk = 0;
   if (remove(fullPath) == 0) {
+    // Just a file.
     isOk = 1;
   } else if (rmdir(fullPath) == 0) {
+    // Empty directory.
+    isOk = 1;
+  } else {
+    // A non-empty directory.
+    pid_t pid = fork();
+    if (pid < 0) {
+      return;
+    }
+    else if (pid == 0) {
+      char* args[] = { "rm", "-rf", fullPath, NULL };
+      execve("/usr/bin/rm", args, NULL);
+      perror(NULL);
+    } else {
+      wait(NULL);
+    }
+
     isOk = 1;
   }
 
   if (isOk) {
     printf("Successfully deleted.\n");
+  } else {
+    printf("Something went wrong!\n");
   }
 }
