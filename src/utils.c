bb374ca94d72af5fe514779deecb01ad3c5f8957
Andrei Gatej <andreigtj01@gmail.com>
2022-01-10 18:20:52 +0200

    chore: add utils

---
+++ src/utils.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int getIndexOf (char* str, char ch) {
+  for (int i = 0, len = strlen(str); i < len; i++) {
+    if (str[i] == ch) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+int equals (char* s1, char* s2) {
+  return !strcmp(s1, s2);
+}
+
+void getKVPairFromRawString (char* rawStr, char** k, char** v, char delimiter) {
+  int delimIdx = getIndexOf(rawStr, delimiter);
+  if (delimIdx == -1) {
+    return;
+  }
+
+  *k = (char*)malloc((delimIdx) * sizeof(char));
+  *v = (char*)malloc((strlen(rawStr) - delimIdx) * sizeof(char));
+
+  strncpy(*k, rawStr, delimIdx);
+  strcpy(*v, rawStr + delimIdx + 1);
+}
---
+++ src/utils.h
@@ -0,0 +1,5 @@
+int getIndexOf(char* str, char ch);
+
+int equals(char *s1, char *s2);
+
+void getKVPairFromRawString(char *rawStr, char** k, char** v, char delimiter);
