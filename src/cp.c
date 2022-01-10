fab7470e2d6dc07754935212571b8d1d8cd287e6
Andrei Gatej <andreigtj01@gmail.com>
2022-01-10 19:48:00 +0200

    fix(cp): add the recursive flag

--- src/cp.c
+++ src/cp.c
@@ -10,22 +10,18 @@ void cpCommand (char* args) {
   char *DEST_BASE = strcat(realpath("dropbox", NULL), "/");
   char* src;
   char* dest;
-  char* absoluteDestPath;
 
   getKVPairFromRawString(args, &src, &dest, ' ');
 
   src = realpath(src, NULL);
   dest = strcat(DEST_BASE, dest);
 
-  printf("src:%s\n", src);
-  printf("dest:%s\n", dest);
-
   pid_t pid = fork();
   if (pid < 0) {
     return;
   }
   else if (pid == 0) {
-    char* args[] = {"cp", src, dest, NULL};
+    char* args[] = {"cp", "-r", src, dest, NULL};
     execve("/usr/bin/cp", args, NULL);
     perror(NULL);
   } else {
