f183e1ee17257709980734d217aa0905b293ec3b
Daria602 <73795793+Daria602@users.noreply.github.com>
2022-01-11 16:20:39 +0200

    feat: add search to the help descr

--- src/help.c
+++ src/help.c
@@ -3,6 +3,6 @@
 void helpCommand()
 {
     char* help_text = 
-    "-------- COMMANDS --------\ncp --> copy files\nls --> list the folders/files inside\nmkdir --> create new folder\nmv --> move folder1/file1 into folder2/file2(creates destination if doesn't exist)\nrm --> remove folder/file\n";
+    "-------- COMMANDS --------\ncp --> copy files\nls --> list the folders/files inside\nmkdir --> create new folder\nmv --> move folder1/file1 into folder2/file2(creates destination if doesn't exist)\nrm --> remove folder/file\nsearch --> search for files/folders\n";
     printf("%s",help_text);
 }
