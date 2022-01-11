e4f66f2f741a1074c9997cac4194f860d9dba0fe
dariaASC <izotovadaria0602@gmail.com>
2022-01-11 07:08:22 -0800

    feat: input interface change

--- main.c
+++ main.c
@@ -4,26 +4,29 @@
 
 #include "src/execute_command.h"
 
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define RESET "\x1B[0m"
+
 int main (int argC, char *args[]) {
   char *rawCommand = NULL;
   size_t len = 0;
   ssize_t read;
 
-  // TODO: ensure `dropbox` created
+ 
 
+  // TODO: ensure `dropbox` created
+  printf(BOLDGREEN "dbxcli > "   RESET);
   while ((read = getline(&rawCommand, &len, stdin)) != -1) {
     if (read <= 0) {
       continue;
     }
-
-    if (strstr(rawCommand, "exit")) {
-      break;
-    }
+    
+    
     if (executeCommand(rawCommand) == 2)
     {
       break;
     }
-    //executeCommand(rawCommand);
+    printf(BOLDGREEN "dbxcli > "   RESET);
   }
 
   free(rawCommand);
